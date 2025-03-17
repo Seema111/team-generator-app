157ad208828bea3010785ca5e8fbe3a15ab8a070
Seema Thapa <seema.thapa@innovatetech.co>
2025-03-18 00:01:40 +0545

    [add] new create page for

---
+++ src/app/create/page.tsx
@@ -0,0 +1,9 @@
+export default function CreatePage() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center space-y-12">
+      <h1 className="text-4xl text-gray-800">
+        Form Here
+      </h1>
+    </div>
+  );
+}
